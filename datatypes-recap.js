9a60acdf62bfa49ae8b3cff7cf070d0fdd8b3072
JAY <iisjaysharma2009@gmail.com>
2023-09-26 22:53:22 +0530

    recaping fluffy things

---
+++ datatypes-recap.js
@@ -0,0 +1,23 @@
+// primitive  data types: number, string, boolean, null, undefined, symbol
+
+let a = 10;
+let b = Symbol("hello");
+let c = true;
+let d = "hello";
+let e = null;
+let f = undefined;
+let g = 10n;
+
+
+// non-primitive data types: object, function, array
+
+let myobj = {
+    name: "John",
+    age: 23
+}; // object
+ 
+let myfunc = function () {
+   console.log("I am a function");
+} // function
+
+let myarr = [1, 2, 3, 4, 5]; // array
